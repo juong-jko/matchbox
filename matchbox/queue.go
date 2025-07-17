7f3bfac09ca93c8df85b3fb024a9ae66c97f9115
juong-jko <juong.jko@gmail.com>
2025-07-17 01:15:37 -0700

    Implement matchmaking queue

--- matchbox/queue.go
+++ matchbox/queue.go
@@ -3,6 +3,32 @@ package matchbox
 type QueueEntry struct {
 	Priority uint64
 	Players  []*Player
+	Index    int
 }
 
 type MatchQueue []*QueueEntry
+
+func (queue *MatchQueue) Len() int {
+	return len(*queue)
+}
+
+func (queue *MatchQueue) Less(i, j int) bool {
+	return (*queue)[i].Priority < (*queue)[j].Priority
+}
+
+func (queue *MatchQueue) Push(x any) {
+	n := len(*queue)
+	item := x.(*QueueEntry)
+	item.Index = n
+	*queue = append(*queue, item)
+}
+
+func (queue *MatchQueue) Pop() any {
+	n := len(*queue)
+	entry := (*queue)[n-1]
+	entry.Index = -1
+
+	(*queue)[n-1] = nil
+	*queue = (*queue)[0 : n-1]
+	return entry
+}
